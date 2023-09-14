d3fb58718f7c2334e9446b2490979671c520f91e
Grober Michael <58160994+leviathan-7@users.noreply.github.com>
2023-09-14 19:10:25 +0300

    Add files via upload
    Cache is made

--- src/cache.js
+++ src/cache.js
@@ -1,4 +1,63 @@
 class Cache{
-
+    constructor() {
+        this.Arr = []
+    }
+    New(Key, Value, TimeLive = 1){
+        let ic = new ItemCache(Key, Value, TimeLive)
+        for (let i = 0; i < this.Arr.length; i++) {
+            if(this.Arr[i].Key === Key){
+                this.Arr[i] = ic
+                return
+            }
+        }
+        this.Arr.push(ic)
+    }
+    Get(Key){
+        for (let i = 0; i < this.Arr.length; i++) {
+            if(this.Arr[i].Key === Key){
+                let v =  this.Arr[i].Value
+                this.Arr[i].TimeLiveMinus()
+                if(this.Arr[i].TimeLive === 0){
+                    this.Arr.splice(i, 1);
+                }
+                return v
+            }
+        }
+        return null
+    }
+    Stat(){
+        let M = []
+        for (let i = 0; i < this.Arr.length; i++) {
+            let T = [this.Arr[i].Key,this.Arr[i].Value,this.Arr[i].TimeLive]
+            M.push(T)
+        }
+        return M
+    }
+    length(){
+        return this.Arr.length
+    }
 }
 export {Cache}
+
+
+class ItemCache{
+    constructor(k, v, t) {
+        this._Key = k
+        this._Value = v
+        this._TimeLive = t
+    }
+    get Key() {
+        return this._Key;
+    }
+    get Value() {
+        return this._Value;
+    }
+    get TimeLive() {
+        return this._TimeLive;
+    }
+    TimeLiveMinus(){
+        this._TimeLive--
+    }
+        
+}
+export {ItemCache}
--- test/cache.test.js
+++ test/cache.test.js
@@ -1,5 +1,53 @@
 import {Cache} from "../src/cache";
 
-test('it fails', () => {
-    expect(false).toBe(true);
+test('Empty length cach = 0', () => {
+    const c = new Cache()
+    expect(c.length()).toBe(0)
+});
+test('length is worked', () => {
+    const c = new Cache()
+    c.New(45,'v1',3)
+    c.New(41,'v2')
+    expect(c.length()).toBe(2)
+});
+test('Rewrite is worked', () => {
+    const c = new Cache()
+    c.New(45,'v1',3)
+    c.New(45,'v2',7)
+    expect(c.length()).toBe(1)
+});
+test('Delete is worked', () => {
+    const c = new Cache()
+    c.New(45,'v1')
+    c.New(42,'v2',7)
+    let a = c.Get(45)
+    expect(c.length()).toBe(1)
+});
+test('No key value is null', () => {
+    const c = new Cache()
+    c.New(45,'v1')
+    expect(c.Get(42)).toBeNull()
+});
+test('TimeLive is 0 then value is null', () => {
+    const c = new Cache()
+    c.New(45,'v1')
+    let a = c.Get(45)
+    expect(c.Get(45)).toBeNull()
+});
+test('Getter is worked', () => {
+    const c = new Cache()
+    c.New(45,'v1')
+    c.New(42,'v2')
+    expect(c.Get(45)).toBe('v1')
+});
+test('Stat is worked', () => {
+    const c = new Cache()
+    c.New(45,'v')
+    c.New(45,'v1',2)
+    c.New(43,'v2',7)
+    c.New(77,'v3')
+    let a = c.Get(43)
+    a = c.Get(77)
+    let M = [[45,'v1',2],[43,'v2',6]]
+    expect(c.Stat()).toEqual(M)
 });
